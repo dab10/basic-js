8c05d7762d3a86a7bbfc1089bebb84b3e77e5005
dab10 <dab10@list.ru>
2022-04-30 17:27:25 +0300

    complete some tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,27 +4,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  str: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
+    return this.str.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value = '') {
+    this.str.push(`( ${value} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    let i = position - 1;
+    if (!Number.isInteger(i) || i < 0 || i >= this.str.length) {
+      this.str = []
+      throw new Error('You can\'t remove incorrect link!')
+    }
+
+    this.str.splice(i, 1)
+    return this
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.str.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let res = this.str.join('~~')
+    this.str = []
+    return res
+  },
 };
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  let arrLocal = [];
+  let discardNext = false;
+  let discardPrev = false;
+ 
+  try {
+    arrLocal = arr.slice();
+
+    for (let i = 0; i < arrLocal.length; i++) {
+      if (arrLocal[i] === '--discard-next' && arrLocal[i + 1] !== undefined) {
+        arrLocal.splice(i, 2)
+        discardNext = true;
+      } else if (arrLocal[i] === '--discard-next' && arrLocal[i + 1] === undefined) {
+        arrLocal.splice(i, 1)
+      } else {
+        continue
+      }
+    }
+
+    for (let i = 0; i < arrLocal.length; i++) {
+      if (arrLocal[i] === '--double-next' && arrLocal[i + 1] !== undefined) {
+        arrLocal.splice(i, 1, arrLocal[i + 1])
+      } else if (arrLocal[i] === '--double-next' && arrLocal[i + 1] === undefined) {
+        arrLocal.splice(i, 1)
+      } else {
+        continue
+      }
+    }
+
+    for (let i = 0; i < arrLocal.length; i++) {
+      if (arrLocal[i] === '--discard-prev' && arrLocal[i - 1] !== undefined && discardNext === false) {
+        arrLocal.splice(i - 1, 2)
+        discardPrev = true
+      } else if ((arrLocal[i] === '--discard-prev' && arrLocal[i - 1] === undefined) || (arrLocal[i] === '--discard-prev' && discardNext === true)) {
+        arrLocal.splice(i, 1)
+      } else {
+        continue
+      }
+    }
+
+    for (let i = 0; i < arrLocal.length; i++) {
+      if (arrLocal[i] === '--double-prev' && arrLocal[i - 1] !== undefined && discardNext === false) {
+        arrLocal.splice(i, 1, arrLocal[i - 1])
+      } else if ((arrLocal[i] === '--double-prev' && arrLocal[i - 1] === undefined) || (arrLocal[i] === '--double-prev' && discardNext === true)) {
+        arrLocal.splice(i, 1)
+      } else {
+        continue
+      }
+    }
+
+    return arrLocal
+  } catch {
+    throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
 }
 
 module.exports = {
