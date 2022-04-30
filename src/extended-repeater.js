4623515f5430afd68fd6772232610111a76f3310
dab10 <dab10@list.ru>
2022-04-30 23:04:42 +0300

    complete task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1,   additionSeparator = '|', }) {
+  let strRes = '';
+  let strAddition = '';
+  let arrAddition = [];
+  let arrRes = [];
+  
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      arrAddition.push(`${addition}`)
+    }
+    strAddition = arrAddition.join(`${additionSeparator}`)
+
+  
+    for (let i = 0; i < repeatTimes; i++) {
+      arrRes.push(`${str}${strAddition}`)
+    }
+    strRes = arrRes.join(`${separator}`)
+    
+    return strRes
+  
 }
 
 module.exports = {
